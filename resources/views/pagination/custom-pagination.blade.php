f45afb6d48a853621785b809171cceb401d2577b
leonaiyem <mdnaiyem.islam.10@gmail.com>
2025-07-19 23:07:45 +0600

    added patinagion in admission page and made some changes in the hms.css

---
+++ resources/views/pagination/custom-pagination.blade.php
@@ -0,0 +1,67 @@
+@if ($paginator->hasPages())
+    <nav>
+        <ul class="pagination justify-content-center gap-1">
+            @if ($paginator->onFirstPage())
+                <li class="page-item disabled">
+                    <span class="page-link w-100 justify-content-center align-items-center" aria-hidden="true">&laquo;</span>
+                </li>
+            @else
+                <li class="page-item">
+                    <a class="page-link w-100 justify-content-center align-items-center" href="{{ $paginator->previousPageUrl() }}" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                    </a>
+                </li>
+
+            @endif
+
+            {{-- pagination links --}}
+            @foreach ($elements as $element)
+                @if (is_string($element))
+                    <li class="page-item disabled">
+                        <span class="page-link w-100 justify-content-center align-items-center">{{ $element }}</span>
+                    </li>
+                @endif
+                @if (is_array($element))
+                    @foreach ($element as $page => $url)
+                        @if ($page == $paginator->currentPage())
+                            <li class="page-item active">
+                                <span style="color: white !important" class="page-link w-100 justify-content-center align-items-center">{{ $page }}</span>
+                            </li>
+
+                        @else
+                            <li class="page-item"><a class="page-link w-100 justify-content-center align-items-center" href="{{ $url }}">{{ $page }}</a></li>
+                        @endif
+                    @endforeach
+                @endif
+            @endforeach
+
+            {{-- next page --}}
+
+            @if ($paginator->hasMorePages())
+                <li class="page-item">
+                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                    </a>
+                </li>
+
+            @else
+                <li class="page-item disabled">
+                    <span class="page-link" aria-hidden="true">&raquo;</span>
+                </li>
+            @endif
+        </ul>
+        <div class="d-none flex-sm-fill d-sm-flex align-items-sm-center justify-content-center">
+            <div>
+                <p class="small text-muted">
+                    {!! __('Showing') !!}
+                    <span class="fw-semibold">{{ $paginator->firstItem() }}</span>
+                    {!! __('to') !!}
+                    <span class="fw-semibold">{{ $paginator->lastItem() }}</span>
+                    {!! __('of') !!}
+                    <span class="fw-semibold">{{ $paginator->total() }}</span>
+                    {!! __('results') !!}
+                </p>
+            </div>
+        </div>
+    </nav>
+@endif
